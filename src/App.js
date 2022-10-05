b8b2816e8d23fae116230463b0d0dce5a37fd787
abi <abiglobal@gmail.com>
2022-10-05 13:53:40 -0700

    Project Setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+function App() {
+  return (
+    <div>
+    <h1>hello</h1>
+
+    </div>
+  );
+}
+
+export default App;
